b8eca67a674e8129e518063ed7aa8a9b819bbee8
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-16 15:03:15 -0500

    chall 1 | Inputs and structure created

---
+++ reto_2_1/index.js
@@ -0,0 +1,7 @@
+function calculate() {
+    // First it evaluated if there are an empty entry
+    var note1 = document.getElementById('note1').value;
+    if (!note1) {
+        window.alert('Requerido');
+    }
+}
