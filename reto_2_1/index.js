c81b401f0c7fdf0f588e35307191f4ae306b327e
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-16 16:05:37 -0500

    Navbar added

--- reto_2_1/index.js
+++ reto_2_1/index.js
@@ -1,7 +1,32 @@
+var M;
+
+document.getElementById('average-calculated').style.visibility = 'hidden';
+document.getElementById('average-calculated').style.display = 'none';
+
+function navegate(valueNavegate) {
+    console.log(valueNavegate);
+    window.location.replace('http://127.0.0.1:5500/index.html')
+}
+
 function calculate() {
-    // First it evaluated if there are an empty entry
+    // First its evaluated if there are an empty entry
     var note1 = document.getElementById('note1').value;
-    if (!note1) {
-        window.alert('Requerido');
+    var note2 = document.getElementById('note2').value;
+    var note3 = document.getElementById('note3').value;
+    var note4 = document.getElementById('note4').value;
+    if (note1 && note2 && note3 && note4) {
+        if (note1 > 0 && note1 < 100 && note2 > 0 && note2 < 100 && note3 > 0 && note3 < 100 && note4 > 0 && note4 < 100 ) {
+            var average = (parseFloat(note1) + parseFloat(note2) + parseFloat(note3) + parseFloat(note4)) / 4;
+            document.getElementById('average-show').innerHTML = average;
+            document.getElementById('average-calculated').style.visibility = 'visible';
+            document.getElementById('average-calculated').style.display = 'block';
+        } else {
+            Materialize.toast('Enter a valid value', 4000);    
+        }
+    } else {
+        Materialize.toast('All notes are required', 4000);
     }
 }
+
+
+
