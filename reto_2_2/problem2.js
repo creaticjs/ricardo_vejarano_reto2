3906aae0b56de8cdf6743bad3e20140af8f77f94
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-16 17:01:28 -0500

    Problem 2 done

--- reto_2_2/problem2.js
+++ reto_2_2/problem2.js
@@ -1,9 +1,27 @@
 function navegate(valueNavegate) {
     console.log(valueNavegate);
-    switch(valueNavegate){
+    switch (valueNavegate) {
         case 1: window.location.replace('http://127.0.0.1:5500/reto_2_1/index.html');
-        break;
+            break;
         case 2: window.location.replace('http://127.0.0.1:5500/reto_2_2/problem2.html')
-        break;
+            break;
+    }
+}
+
+function calculate() {
+    var g = 6.673e-8;
+    var m1 = document.getElementById('masse1').value;
+    var m2 = document.getElementById('masse2').value;
+    var dis = document.getElementById('distance').value;
+    if (m1 && m2 && dis) {
+        dis2 = Math.pow(dis, 2)
+        var f = (g * m1 * m2) / dis2;
+        document.getElementById('force-show').innerHTML = '<p>Force: ' +f + '  g × cm/seg² </p>';
+
+        var fString = f.toString().replace('e', 'x10')
+
+        document.getElementById('force-show2').innerHTML ='<p>Force in dynas: ' + fString + '  dyn </p>';
+    } else {
+        Materialize.toast('All fields are required', 4000);
     }
 }
