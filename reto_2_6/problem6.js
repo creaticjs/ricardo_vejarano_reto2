9b60fc2043a1fb899835e79e27b39297af3ca5a8
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2019-03-17 17:19:09 -0500

    Problem 6 done

--- reto_2_6/problem6.js
+++ reto_2_6/problem6.js
@@ -16,10 +16,48 @@ function calculate() {
 
 function determinateHour(hour) {
     var temp = hour.split(':');
-    if(temp[0] > 0 && temp[0] <= 24  ) {
+    if(temp[0] >= 0 && temp[0] < 24  && temp[1] > 0 && temp[1] < 60) {
+
+        if(temp[0] > 12) {
+            var tempHour = '0';
+            switch(temp[0]) {
+                case '13': tempHour = '01';
+                break;
+                case '14': tempHour = '02';
+                break;
+                case '15': tempHour = '03';
+                break;
+                case '16': tempHour = '04';
+                break;
+                case '17': tempHour = '05';
+                break;
+                case '18': tempHour = '06';
+                break;
+                case '19': tempHour = '07';
+                break;
+                case '20': tempHour = '08';
+                break;
+                case '21': tempHour = '09';
+                break;
+                case '22': tempHour = '10';
+                break;
+                case '23': tempHour = '11';
+                break;
+                case '24': tempHour = '12';
+                break;
+            }
+            document.getElementById('time').innerHTML = '<p>Time: ' + tempHour + ':' + temp[1] + ' pm</p>';
+        } else {
+            // Same hour
+            if(temp[0] !== '00') {
+                document.getElementById('time').innerHTML = '<p>Time: ' + temp[0] + ':' + temp[1] + ' am</p>';
+            } else {
+                document.getElementById('time').innerHTML = '<p>Time: ' + '12' + ':' + temp[1] + ' am</p>';
+            }
+            
+        }
 
     } else {
         Materialize.toast('Invalid hour', 4000);
     }
-    console.log(hour);
 }
